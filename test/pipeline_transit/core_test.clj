f2e39b5abf2e39c79861d84c40f4e7ffb80690b7
Paul Lam <paul@quantisan.com>
2021-05-07 19:36:37 +0900

    test check for nil value

--- test/pipeline_transit/core_test.clj
+++ test/pipeline_transit/core_test.clj
@@ -24,9 +24,11 @@
      [:foo 3 "c"]
      :foo
      {:foo :bar}
+     nil
 
      {:tick/date-time     #time/date-time "2019-11-01T02:00"
       :tick/time          #time/time "08:12:13.366"
       :clj-time/date-time (org.joda.time.DateTime. 2021 11 1 2 0 (org.joda.time.DateTimeZone/UTC))
-      :uuid               (java.util.UUID/fromString "b5b5cabc-7bfe-4c33-a6a2-f0671eb913c8")}))
+      :uuid               (java.util.UUID/fromString "b5b5cabc-7bfe-4c33-a6a2-f0671eb913c8")
+      :nil                nil}))
 
